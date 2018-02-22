708393fd698e854ca0e9b79fe46111f1b3b66bfd
sds0208 <sarahsantacruz86@gmail.com>
2018-02-21 23:47:06 -0600

    html/css structure added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Critique My Performance</h1>
         </header>
+        <video autoPlay muted loop id="myVideo">
+          <source src="assets/Playing_Acoustic_Guitar.mp4" type="video/mp4" />
+        </video>
+        <div className="sign-up">
+          <p>Sign up today to get feedback from musicians around the world! Already have an account? Log in.</p>
+          <form className="sign-up-form">
+            Email:
+            <input type="text" /><br></br>
+            Username:
+            <input type="text" /><br></br>
+            Password:
+            <input type="text" /><br></br>
+            Confirm Password:
+            <input type="text" />
+            <button>Submit</button>
+          </form>
+        </div>
         <p className="App-intro">
           A community of musicians helping musicians.
         </p>

50363a54173ea7fbedf20899232b6cedb6fa57c4
sds0208 <sarahsantacruz86@gmail.com>
2018-03-04 22:21:16 -0600

    react-iframe installed, sample youtube and soundcloud added

--- src/components/Studio.js
+++ src/components/Studio.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import Iframe from 'react-iframe';
 
 class Studio extends Component {
   render() {
     return (
       <div className="Studio">
-        Studio content will go here.
+        <div className="video-div">
+          <Iframe url="https://www.youtube.com/embed/UN6_A_QyigU"
+          width="560" height="315"
+          position="static"
+          className="video"
+          allowfullscreen />
+        </div>
+        <div className="video-div">
+          <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/130628931&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          width="560" height="315"
+          position="static"
+          className="video" />
+        </div>
       </div>
     );
   }

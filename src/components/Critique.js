c6dfee2dc493c2d718d2f337eed780795b467402
sds0208 <sarahsantacruz86@gmail.com>
2018-03-17 12:50:32 -0500

    critique functionality started, but not working yet

--- src/components/Critique.js
+++ src/components/Critique.js
@@ -3,27 +3,43 @@ import React, { Component } from 'react';
 class Critique extends Component {
   constructor(props) {
     super(props);
-    this.state = { content: '', timeAdded: [], addedBy: '' };
+    this.state = { content: '', timeAdded: [], addedBy: '', critiques: [] };
+    this.critiquesRef = this.props.firebase.database().ref('critiques');
     this.handleCritique = this.handleCritique.bind(this);
-    this.postCritique = this.postCritique.bind(this);
+    this.pushCritique = this.pushCritique.bind(this);
   }
 
-  //Users will add critique that will belong to the specific video it was added to.
+  componentDidMount() {
+    this.critiquesRef.on('child_added', snapshot => {
+      const critique = snapshot.val();
+      critique.key = snapshot.key;
+      this.setState({ critiques: this.state.critiques.concat( critique ) });
+    });
+  }
+
+  //Users can add critique that will belong to the specific video it was added to.
   //Critique will be displayed below each video for that specific video.
 
   handleCritique(event) {
     event.preventDefault();
-    this.setState({ critique: event.target.value });
+    this.setState({ content: event.target.value });
   }
 
-  postCritique() {
-    this.props.pushCritique(this.state.content);
+  pushCritique(content) {
+    console.log(this.state.content);
+    const date = new Date();
+    this.critiquesRef.push({
+      content: content,
+      timeAdded: [date.toLocaleDateString(), date.toLocaleTimeString()],
+      iframeID: this.props.activeIframe.key,
+      addedBy: this.props.user
+    });
   }
 
   render() {
     return(
       <div className="Critique">
-        <form onSubmit={() => this.postCritique()}>
+        <form onSubmit={() => this.pushCritique(this.state.content)}>
           <input className="profile-input" type="text" value={this.state.content} onChange={this.handleCritique}></input>
           <button className="button" type="submit">Critique</button>
         </form>
--- src/components/Studio.js
+++ src/components/Studio.js
@@ -6,11 +6,11 @@ import Critique from './Critique';
 class Studio extends Component {
   constructor(props) {
     super(props);
-    this.state = { newIframe: '', iframes: [] };
+    this.state = { newIframe: '', iframes: [], activeIframe: '' };
     this.iframesRef = this.props.firebase.database().ref('iframes');
     this.postVideo = this.postVideo.bind(this);
     this.handleIframe = this.handleIframe.bind(this);
-    this.pushCritique = this.pushCritique.bind(this);
+    this.activateIframe = this.activateIframe.bind(this);
   }
 
   componentDidMount() {
@@ -29,22 +29,21 @@ class Studio extends Component {
         timeAdded: [date.toLocaleDateString(), date.toLocaleTimeString()],
         userUID: this.props.user.uid,
         userName: this.props.user.displayName,
-        userEmail: this.props.user.email,
-        critiques: []
+        userEmail: this.props.user.email
       });
     } else {
       alert("This is not a valid YouTube or SOUNDCLOUD iframe. Please try again.");
     }
   }
 
-  pushCritique(content) {
-    const date = new Date();
-    this.iframesRef.push({ critiques: {content: content, timeAdded: [date.toLocaleDateString(), date.toLocaleTimeString()], addedBy: this.props.user.displayName || this.props.user.email} });
-  }
-
   handleIframe(event) {
     event.preventDefault();
     this.setState({ newIframe: event.target.value.replace("></iframe>", "/>") });
+  }
+
+  activateIframe(iframe) {
+    this.state.activeIframe !== iframe ? this.setState({ activeIframe: iframe }) : this.setState({ activeIframe: '' });
+    console.log(this.state.activeIframe);
   }
 
   render() {
@@ -60,14 +59,18 @@ class Studio extends Component {
             <li>Copy and paste the iframe here:</li>
           </ul>
           <form onSubmit={() => this.postVideo()}>
-            <input className="profile-input" ype="text" value={this.state.newIframe} onChange={this.handleIframe}></input>
+            <input className="profile-input" type="text" value={this.state.newIframe} onChange={this.handleIframe}></input>
             <button className="button" type="submit">Post</button>
           </form>
           {this.state.iframes.map(iframe =>
             <div key={iframe.key} className="video">
               <h5>Posted by {iframe.userName || iframe.userEmail} on {iframe.timeAdded[0]} at {iframe.timeAdded[1]}</h5>
               {ReactHtmlParser(iframe.iframe)}
-              <Critique pushCritique={this.pushCritique.bind(this)} />
+              <button className={this.state.activeIframe.key === iframe.key ? 'no-show' : "button"} onClick={() => this.activateIframe(iframe)}>Critique</button>
+              {this.state.activeIframe.key === iframe.key ?
+                <Critique firebase={this.props.firebase} activeIframe={this.state.activeIframe} activateIframe={this.activateIframe.bind(this)} user={this.props.user}/> : null
+              }
+
             </div>
           )}
 

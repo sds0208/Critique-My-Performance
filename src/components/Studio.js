c5d7b384171b1d23574bbda6b3c9820aac4b3cde
sds0208 <sarahsantacruz86@gmail.com>
2018-03-26 19:05:56 -0500

    user guidelines displayed on studio page, users can report violations, users can be removed in database

--- src/components/Studio.js
+++ src/components/Studio.js
@@ -12,13 +12,26 @@ class Studio extends Component {
         <div className="Studio">
           <SignOut firebase={this.props.firebase} user={this.props.user}/>
           <h1>The Studio</h1>
-          <p>To post your performance:</p>
-          <ul>
-            <li>Create a YouTube Video or SOUNDCLOUD Track.</li>
-            <li>Click the Share button at the bottom of the video or track.</li>
-            <li>Select the "Embed" option.</li>
-            <li>Copy and paste the iframe here:</li>
-          </ul>
+          <div className="instructions-container">
+            <div className="instructions">
+              <h4>Guidelines for Performances and Critique:</h4>
+              <ul>
+                <li>Content must be appropriate for all audiences.</li>
+                <li>Only helpful critique is allowed. Avoid mean or discouraging remarks.</li>
+                <li>Any users who do not follow these guidelines will be removed, along with their content.</li>
+                <li>Report any violations by emailing critiquemyperformance@gmail.com</li>
+              </ul>
+            </div>
+            <div className="instructions">
+              <h4>To post your performance:</h4>
+              <ul>
+                <li>Create a YouTube Video or SOUNDCLOUD Track.</li>
+                <li>Click the Share button at the bottom of the video or track.</li>
+                <li>Select the "Embed" option.</li>
+                <li>Copy and paste the iframe here:</li>
+              </ul>
+            </div>
+          </div>
           < Performances firebase={this.props.firebase} activateIframe={this.props.activateIframe.bind(this)} activeIframe={this.props.activeIframe} user={this.props.user}/>
         </div> : null
     );
